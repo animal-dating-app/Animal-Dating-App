5940c5664a012007b05ee05a5af806d3d57dca04
Teresita Guzman Nader <TeresitaCGNader@users.noreply.github.com>
2023-12-01 22:36:53 -0800

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar.In";
 import Footer from "./components/Footers/Footer.js";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./pages";
+import Pets from "./pages/pets.js";
+import Events from "./pages/events";
+import SignUp from "./pages/signup";
 
 function App() {
     return (
-        <div className="App">
-            <h1>Dating App for Animal Adoption</h1>
-            <p>Coming Soon!</p>
-            <Footer />
-        </div>
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/pets" element={<Pets />} />
+                    <Route path="/events" element={<Events />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                </Routes>
+                <h1>Dating App for Animal Adoption</h1>
+                <p>Coming Soon!</p>
+                <Footer />
+            </div>
+        </Router>
     );
 }
 
