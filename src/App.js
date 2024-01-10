1483ab84d47f18377d5b36ed714f06076b5b4acc
Teresita Guzman Nader <TeresitaCGNader@users.noreply.github.com>
2024-01-09 18:35:13 -0800

    Deleted Events page and all elements related to it

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import {
 } from "react-router-dom";
 import Home from "./pages";
 import Pets from "./pages/pets.js";
-import Events from "./pages/events";
 import SignUp from "./pages/signup";
 
 function App() {
@@ -20,7 +19,6 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/pets" element={<Pets />} />
-                    <Route path="/events" element={<Events />} />
                     <Route path="/sign-up" element={<SignUp />} />
                 </Routes>
                 <h1>Dating App for Animal Adoption</h1>
