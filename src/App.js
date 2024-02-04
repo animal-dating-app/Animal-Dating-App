cfadd7a619f59976cd4c4d769cc2f1636ce1b374
Carter Roeser <carter@cdgtech.one>
2024-02-03 21:19:50 -0800

    Fix Loading Auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import { auth } from "./firebaseConfig";
 import Navbar from "./components/Navbar.In";
 import Footer from "./components/Footers/Footer.js";
 import {
@@ -8,6 +7,7 @@ import {
     Routes,
     Route,
 } from "react-router-dom";
+import { UserProvider, useUser } from './UserContext';
 import Home from "./pages/home.js";
 import Pets from "./pages/pets.js";
 import SignUp from "./pages/signup";
@@ -17,46 +17,40 @@ import Pet from "./pages/pet.js";
 import FullScreenLoader from './components/FullScreenLoader';
 import Header from './components/Header/Header.js'
 
-
 function App() {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [isFadingOut, setIsFadingOut] = useState(false);
+    return (
+        <UserProvider>
+            <Router>
+                <div className="App">
+                    <AuthenticatedApp />
+                </div>
+            </Router>
+        </UserProvider>
+    );
+}
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            setUser(user);
-            setTimeout(() => {
-                setIsFadingOut(true);
-            }, 200); // Always show the loader for at least 200ms
+function AuthenticatedApp() {
+    const { user, userLoaded, showUserLoader, startFading } = useUser();
 
-            setTimeout(() => {
-                setLoading(false);
-            }, 1200); // Delay + duration of the fade out animation
-        });
-
-        return () => unsubscribe();
-    }, []);
+    if (!userLoaded) { return <FullScreenLoader />}
 
     return (
-        <Router>
-            <div className="App">
-                {loading && <FullScreenLoader fadingOut={isFadingOut} />}
-                <Header /> 
-                <Navbar user={user} />
-                <div className="pageWrapper">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/pets" element={<Pets />} />
-                        <Route path="/dashboard" element={<Dashboard />} />
-                        <Route path="/sign-up" element={<SignUp user={user} />} />
-                        <Route path="/sign-in" element={<SignIn user={user} />} />
-                        <Route path="/pet" element={<Pet />} />
-                    </Routes>
-                </div>
-                <Footer />
+        <>
+            { showUserLoader && <FullScreenLoader fadingOut={startFading} /> }
+            <Header />
+            <Navbar user={user} />
+            <div className="pageWrapper">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/pets" element={<Pets />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/sign-up" element={<SignUp user={user} />} />
+                    <Route path="/sign-in" element={<SignIn user={user} />} />
+                    <Route path="/pet" element={<Pet />} />
+                </Routes>
             </div>
-        </Router>
+            <Footer />
+        </>
     );
 }
 
