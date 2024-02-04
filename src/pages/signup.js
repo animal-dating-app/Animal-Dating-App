cc20c20a98b4538355fc8d6852c63f810dc608f3
Nick Francisco <francnic@oregonstate.edu>
2024-02-04 15:06:59 -0800

    Sign Up v0.5 - get info and save to database

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,92 +1,43 @@
 import React, { useState } from "react";
-import { auth } from "../firebaseConfig";
-import {  createUserWithEmailAndPassword  } from 'firebase/auth';
 import { NavLink, useNavigate } from 'react-router-dom';
+import SignUpUser from "../components/Cards/SignUpUser";
+import SignUpShelter from "../components/Cards/SignUpShelter";
  
 const SignUp = ({ user }) => {
 
     const navigate = useNavigate();
-
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('');
+    const [isShelter, setIsShelter] = useState(false);
 
     if (user) navigate("/");
- 
-    const onSubmit = async (e) => {
-      e.preventDefault()
-     
-      await createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in
-            //const user = userCredential.user;
-            //console.log(user);
-            navigate("/")
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode, errorMessage);
-            // ..
-        });
+    
+    const handleRadioChange = (e) => {
+        const value = e.target.id;
+        if (value === "shelter") {
+            setIsShelter(true);
+        }
+        else {
+            setIsShelter(false);
+        }
     }
 
     return (
-        <div>
-            <section>
-            <div>
-                <div>                  
-                    <h1> Create Account</h1>
-                    <br></br>                                                                            
-                    <form>                                                                                            
-                        <div>
-                            <label htmlFor="email-address">
-                                Email Address
-                            </label>
-                            <br></br>
-                            <input
-                                type="email"
-                                label="Email Address"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}  
-                                required                                    
-                                placeholder="Email Address"                                
-                            />
-                        </div>
-
-                        <div>
-                            <label htmlFor="password">
-                                Password
-                            </label>
-                            <br></br>
-                            <input
-                                type="password"
-                                label="Create password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)} 
-                                required                                 
-                                placeholder="Password"              
-                            />
-                        </div>                                             
-                        <br></br>
-                        <button
-                            type="submit" 
-                            onClick={onSubmit}                        
-                        >  
-                            Create Account                               
-                        </button>
-                                                                     
-                    </form>
-                   
-                    <p>
-                        Already have an account?{' '}
-                        <NavLink to="/sign-in" >
-                            Sign in
-                        </NavLink>
-                    </p>                   
+        <div>                      
+            <h1> Create Account</h1>
+            <form>             
+                <div>
+                    <input type="radio" className="btn-check m-2" name="options-base" id="adopt" autoComplete="off" defaultChecked onClick={handleRadioChange}/>
+                    <label className="btn m-2" htmlFor="adopt">Adopt</label>
+                    <input type="radio" className="btn-check m-2" name="options-base" id="shelter" autoComplete="off" onClick={handleRadioChange} />
+                    <label className="btn m-2" htmlFor="shelter">Shelter</label>
                 </div>
-            </div>
-        </section>
+                {isShelter && (
+                    <SignUpShelter />
+                )}
+                {!isShelter && (
+                    <SignUpUser />
+                )}
+                <p>Already have an account?{' '}<NavLink to="/sign-in" >Sign in</NavLink></p>                                           
+            </form> 
         </div>
     );
 };
