f4d66b2919dfe388f0db1782b04e9768e6727217
Teresita Guzman Nader <TeresitaCGNader@users.noreply.github.com>
2024-02-17 14:03:12 -0800

    Finished 404 page

--- src/App.js
+++ src/App.js
@@ -18,9 +18,6 @@ import FullScreenLoader from './components/FullScreenLoader';
 import Header from './components/Header/Header.js';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Cats from "./pages/pets/cats.js";
-import Dogs from "./pages/pets/dogs.js";
-import Others from "./pages/pets/others.js";
 import PageNotFound from "./components/404Page/404Page.js";
 function App() {
     return (
@@ -48,11 +45,7 @@ function AuthenticatedApp() {
             <div className="pageWrapper">
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/pets" element={<Pets />}>
-                        <Route path="cats" element={<Cats />} />
-                        <Route path="dogs" element={<Dogs />} />
-                        <Route path="others" element={<Others />} />
-                    </Route>
+                    <Route path="/pets" element={<Pets />} />
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/sign-up" element={<SignUp user={user} />} />
                     <Route path="/sign-in" element={<SignIn user={user} />} />
